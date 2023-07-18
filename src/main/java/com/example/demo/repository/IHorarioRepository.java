a6727c2c032f7406958295c12becd175ffea3138
Xavier Aguilar <kevinjkevps4@gmail.com>
2023-07-17 22:39:27 -0500

    tarea 14 servicio horario

---
+++ src/main/java/com/example/demo/repository/IHorarioRepository.java
@@ -0,0 +1,25 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import com.example.demo.repository.modelo.Horario;
+
+public interface IHorarioRepository {
+	
+	public Horario seleccionarPorCodigo(String codigo);
+
+	public void insertar(Horario horario);
+
+	public void actualizar(Horario horario);
+
+	public void actualizarParcial(String nombreActual, String nombreNuevo);
+
+	public void borrar(Integer id);
+
+//	public List<Horario> buscarTodosProvincia(String provincia);
+
+	public List<Horario> buscarTodos();
+
+	public Horario buscarID(Integer id);
+
+}
